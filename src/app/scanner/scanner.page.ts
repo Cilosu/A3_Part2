4845a7c11512bdad4f07580b62fe59eab5ae417e
Cherry1315 <c.linkebasic@gmail.com>
2023-12-06 15:22:47 +1100

    barcode scanner plugin impl;mented
    only the bare bones (basic functionality). still needs comments and everything else in-between etc etc

--- src/app/scanner/scanner.page.ts
+++ src/app/scanner/scanner.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {ScannerPageRoutingModule} from "./scanner-routing.module";
 
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 @Component({
   selector: 'app-scanner',
   templateUrl: 'scanner.page.html',
@@ -8,6 +9,15 @@ import {ScannerPageRoutingModule} from "./scanner-routing.module";
 })
 export class ScannerPage {
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner) {}
+
+  scanBarcode(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      alert(barcodeData);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
 
 }
