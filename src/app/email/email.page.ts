9698cc314b7756bb95f3cd20184589b48ec25229
Cherry1315 <c.linkebasic@gmail.com>
2023-12-07 23:21:28 +1100

    code clean up
    code cleanup

--- src/app/email/email.page.ts
+++ src/app/email/email.page.ts
@@ -42,25 +42,3 @@ export class EmailPage {
   }
 
 }
-
-
-// sendEmail(){
-//   let email = {
-//     to: 'max@mustermann.de',
-//     cc: 'erika@mustermann.de',
-//     bcc: ['john@doe.com', 'jane@doe.com'],
-//     attachments: [
-//       // 'file://img/logo.png',
-//       // 'res://icon.png',
-//       // 'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-//       // 'file://README.pdf'
-//     ],
-//     subject: 'Cordova Icons',
-//     body: 'How are you? Nice greetings from Leipzig',
-//     isHtml: true
-//   }
-//
-// // Send a text message using default options
-//   this.emailComposer.open(email);
-//
-// }
